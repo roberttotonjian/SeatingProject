820a4ad2198dc2349e85420c528bd5c8f84d4342
unknown <robert.totonjian@IL_EZIO.mel.int.omnilab.com.au>
2013-04-12 17:19:22 +1000

    added mulltidimensional array and string based things

--- arraytest.py
+++ arraytest.py
@@ -1,9 +1,19 @@
-dbarray = [None] * 100
-print dbarray[5]
+master = [None] * 100
+print master[5]
 for i in range(100):
 	j = i*i
-	dbarray[i] = [j, j+1, j+2]
+	master[i] = [j, j+1, j+2]
 
 for x in range(10):
-	print dbarray[x][2]
-	print dbarray[x][1]
+	print master[x][2]
+	print master[x][1]
+
+
+user = [None]
+computer = [None]
+seat = [None]
+
+user = ['adam', 'toto', 'rodney', 'jon']
+print user[1]
+user = [[1110, 'adam'], [1111, 'toto'], [1112, 'rodney'], [1113, 'jon']]
+print user[1][0]
