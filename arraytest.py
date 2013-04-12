919383c4c19d3af6cbf4a459af9040a94005f1b2
toto <robert.totonjian@gmail.com>
2013-04-12 17:27:32 +1000

    filled out arrays for seat and comp also changed array names for seat and comp

--- arraytest.py
+++ arraytest.py
@@ -9,11 +9,8 @@ for x in range(10):
 	print master[x][1]
 
 
-user = [None]
-computer = [None]
-seat = [None]
-
-user = ['adam', 'toto', 'rodney', 'jon']
-print user[1]
 user = [[1110, 'adam'], [1111, 'toto'], [1112, 'rodney'], [1113, 'jon']]
+comp = [['adam\'s_comp', 'Z400'], ['toto\'s_Comp', 'T5500'], ['rodneyComp01' 'Z400'], ['jonbox_01', 'Z600']]
+seat = [[0, 0], [0, 1], [0, 2], [1, 0]]
 print user[1][0]
+print comp[2][0]
