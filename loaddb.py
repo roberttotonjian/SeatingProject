c07375f10a412f94b2e1f3034144f0c6bb83cb8c
unknown <robert.totonjian@IL_EZIO.mel.int.omnilab.com.au>
2013-04-16 17:09:19 +1000

    added more loading funct

--- loaddb.py
+++ loaddb.py
@@ -1,5 +1,25 @@
 from seatingfuncts import *
-import json
+import csv
+
+global alph
+global user
+global comp
+global seat
+global ram
+global complist
+global master
+global dblist
+
+alph = []
+user = []
+comp = []
+seat = []
+ram = []
+complist = []
+dblist = []
+alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+master = [[]]
+
 
 # loads list into master array <name of array to impoort> <position of data in list to import> <position to import to>
 def loadtomaster(name, posim, posma):
@@ -20,50 +40,78 @@ def loadseattomaster(name, posim1, posim2, posma1, posma2):
 #prints all content of master db
 def printmaster():
 	for i in range(len(master)):
-		print(str(i), str(master[i]))
+		print('User:'+str(i)+' - '+str(master[i]))
 
-def savedb(name, filename):
-	f = open(str(filename), 'w')
-	f.write(str(name))
-	f.close()
+def savedb(name, strname):
+	f = open('db/'+strname, 'w')
+	for i in name:
+		temp = str(name[i][0])+', '+str(name[i][1])
 
-# def loaddb(name, filename):
-# 	f = open(str(filename), 'r')
-# 	name = f.readline(0)
-# 	print(name)
+def loaddblist():
+	reader = csv.reader(open('db/list', 'rb'))
+	for record in reader:
+		record = str(record).replace("[", "")
+		record = str(record).replace("]", "")
+		record = str(record).replace("'", "")
+		record = str(record).replace("'", "")
+		dblist.append(record)
+
+
+def loaddb():
+	for each in dblist:
+		reader = csv.reader(open('db/'+each, 'rb'))
+		for record in reader:
+			# print(record)
+			user.append(record)
+
+# user = [[1110, 'Adam'], [1111, 'Toto'], [1112, 'Rodney'], [1113, 'Jon']]
+# comp = [[1110, 1], [1111, 4], [1112, 3], [1113, 3]]
+# seat = [[1110, 1, 1], [1111, 6, 2], [1112, 4, 3], [1113, 4, 5]]
+# ram = [[1110, 20], [1111, 12], [1112, 8], [1113, 16]]
+# complist = [[0, 'Earlier Crap'], [1, 'Z400'], [2, 'Z600'], [3, 'Z620'], [4, 'T5400'], [5, 'T5500']]
+
+
+loaddblist()
+loaddb()
+
+print(dblist)
+
+print(user)
+
+# for db in list(dblist):
+# 	print db
+# 	f = open('db/'+str(db), 'r')
+# 	db = [item.strip('\n') line.split(',') for line in open('db/'+str(db))]
 # 	f.close()
 
-global alph
-alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-global user
-user = [[1110, 'Adam'], [1111, 'Toto'], [1112, 'Rodney'], [1113, 'Jon']]
-global comp
-comp = [[1110, 1], [1111, 4], [1112, 3], [1113, 3]]
-global seat
-seat = [[1110, 1, 1], [1111, 6, 2], [1112, 4, 3], [1113, 4, 5]]
-global ram
-ram = [[1110, 20], [1111, 12], [1112, 8], [1113, 16]]
-global complist
-complist = [[0, 'Earlier Crap'], [1, 'Z400'], [2, 'Z600'], [3, 'Z620'], [4, 'T5400'], [5, 'T5500']]
-global master
-master = [[]]
 
-loaddb(user, "user")
 
-records = 1
-for i in range(len(user)):
-	if user[i][0] > records:
-		records = user[i][0]
-records = records+1
-# master = str(range(records))
 
-for i in range(records):
-	master.append([None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None])
+# records = 1
+# for i in range(len(list(user))):
+# 	if user[i][0] > records:
+# 		records = user[i][0]
+# records = records+1
+# # master = str(range(records))
 
-loadtomaster(user, 1, 0)
-loadtomaster(seat, 1, 1)
-loadseattomaster(seat, 1, 2, 2, 3)
-loadtomaster(ram, 1, 4)
+# for i in range(records):
+# 	master.append([None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None])
+
+# loadtomaster(user, 1, 0)
+# loadtomaster(seat, 1, 1)
+# loadseattomaster(seat, 1, 2, 2, 3)
+# loadtomaster(ram, 1, 4)
+
 
 # printmaster()
-# savedb(user, "user")
+
+
+
+
+
+
+
+
+
+
+
