8caa663646aa033fb807722816bc3172c7eae82f
unknown <robert.totonjian@IL_EZIO.mel.int.omnilab.com.au>
2013-04-19 12:50:03 +1000

    fixed saving/loading

--- loaddb.py
+++ loaddb.py
@@ -8,21 +8,35 @@ alph = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 # loads/reloads the master db from files
 def reloaddb():
+	global listcount
+	global dblist
+	listcount = 0
+	dblist = []
 	loaddb()
 	buildrecordlist()
 	initmaster()
-	loadtomaster(user, 1, 0)
-	loadtomaster(seat, 1, 1)
-	loadseattomaster(seat, 1, 2, 2, 3)
-	loadtomaster(ram, 1, 4)
+	loadtomaster(user, "user", 1, 0)
+	loadtomaster(seat2, "seat1", 1, 1)
+	loadtomaster(seat1, "seat2", 1, 2)
+	loadtomaster(ram, "ram", 1, 3)
+	loadtomaster(comp, "comp", 1, 4)
 
 
 # loads list into master array
-def loadtomaster(name, posim, posma):
+def loadtomaster(name, dbname, posim, posma):
+	global listcount
+	global dblist
+	dblist.append(['-', '-', '-'])
+	dblist[listcount][0] = dbname
+	dblist[listcount][1] = posim
+	dblist[listcount][2] = posma
+	# dblist.append(str(dbname)+', '+str(posim)+', '+str(posma))
 	for i in range(len(name)):
 		uid = int(name[i][0])
 		cont = name[i][posim]
 		master[uid][posma] = cont
+	listcount = listcount+1
+
 
 # loads seat list into array and swaps out number for letter
 def loadseattomaster(name, posim1, posim2, posma1, posma2):
@@ -33,16 +47,31 @@ def loadseattomaster(name, posim1, posim2, posma1, posma2):
 		master[uid][posma1] = str(alph[int(cont1)])
 		master[uid][posma2] = cont2
 
+
 # prints all content of master db
 def printmaster():
 	for i in range(len(master)):
 		print('User:'+str(i)+' - '+str(master[i]))
 
+
 # saves master db back into individual lists
-def savedb(name, strname):
-	f = open('dbtest/'+strname, 'w')
-	for i in name:
-		temp = str(name[i][0])+', '+str(name[i][1])
+def savedb():
+	global eachdb
+	global eachrec
+	for eachdb in dblist:
+		myfile = open("db/"+str(eachdb[0]), "w")
+		myfile.write("")
+		for eachrec in range(len(list(master))):
+			if master[eachrec][0] != "-":
+				print("-----"+str(eachrec))
+				print("-----"+str(eachdb))
+				dbname = eachdb[0]
+				dbrec = str(master[eachrec][int(eachdb[2])])
+				myfile.write(str(eachrec)+','+str(dbrec)+'\n')
+				# # eachdb[0]+'.append('+str(eachrec)+','+str(master[eachrec][int(eachdb[2])]+')')
+				# print("global dbname; global eachrec; global dbrec; "+str(dbname)+".append("+str(eachrec)+','+str(dbrec)+")")
+
+
 
 # loads all the individual dbs into their respective lists
 def loaddb():
@@ -50,10 +79,9 @@ def loaddb():
 		reader = csv.reader(open('db/'+each, 'rb'))
 		templist = []
 		for record in reader:
-			# print(record)
 			templist.append(record)
 		exec("global "+str(each)+";"+str(each)+" = templist")
-		# exec("print("+str(each)+")")
+
 
 # prepares the master database to be populated
 def initmaster():
@@ -64,7 +92,7 @@ def initmaster():
 		empty = empty+'-'
 	for i in range(records):
 		master.append(list(empty))
-		# print(range(records))
+
 
 # calculates how many records to create in the master db
 def buildrecordlist():
@@ -77,4 +105,5 @@ def buildrecordlist():
 
 
 reloaddb()
+savedb()
 printmaster()
